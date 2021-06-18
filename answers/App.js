f1822c5f440466c0d6d538b1067e80465f288162
Academy Xi <academyxi@Academys-MacBook-Air.local>
2021-06-18 13:35:40 +1000

    added answer files

---
+++ answers/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { DogContainer } from './containers/dogContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <DogContainer />
+    </div>
+  );
+}
+
+export default App;
